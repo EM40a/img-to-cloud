5e555353111a8439bc157d1cf0cea66d3900c983
Emanuel Hermosilla <hermosilla.emanuel811@gmail.com>
2024-03-04 12:25:25 -0300

    fix(button): overflow issue

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -26,24 +26,24 @@ export default function Button({ image, loading }) {
         <button
           name="submit"
           type="submit"
+          aria-label="Generate link button"
           className="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-8 text-sm font-medium text-center text-white bg-blue-700 dark:bg-blue-600 border hover:bg-blue-800 dark:hover:bg-blue-700 rounded-s-lg border-blue-700 dark:border-blue-600 hover:border-blue-700 dark:hover:border-blue-700 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
         >
           Generate
         </button>
-        <div className="relative w-full overflow-clip">
-          <label
-            htmlFor="submit"
-            aria-describedby="helper-text-explanation"
-            className={`bg-gray-50 border border-e-0 border-gray-300 text-gray-500 dark:text-gray-400 text-sm border-s-0 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 ${
-              loading && "animate-pulse"
-            }`}
-            readOnly
-            disabled
-          >
-            {loading ? "Loading..." : image}
-          </label>
-        </div>
+        <label
+          htmlFor="submit"
+          aria-describedby="helper-text-explanation"
+          className={`w-full truncate bg-gray-50 border border-e-0 border-gray-300 text-gray-500 dark:text-gray-300 text-sm border-s-0 focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 ${
+            loading && "animate-pulse"
+          }`}
+          readOnly
+          disabled
+        >
+          {loading ? "Loading..." : image}
+        </label>
         <button
+          aria-label="Copy to clipboard"
           name="copy"
           id="copy"
           className='lex-shrink-0 py-3 px-4 text-gray-500 bg-gray-100 border border-gray-300 rounded-e-lg  dark:bg-gray-700 dark:border-gray-600 "hover:bg-gray-200 focus:outline-none focus:ring-gray-100 hover:text-blue-600 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:hover:text-blue-500 dark:text-gray-400'
@@ -79,7 +79,6 @@ export default function Button({ image, loading }) {
             </svg>
           </span>
         </button>
-        <div className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"></div>
       </div>
 
       <p className="mt-2 text-sm text-gray-500 dark:text-gray-400">
